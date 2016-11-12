ad9f781b6de03948e775919b2a2192e9ab26b27d
Tommy Li <tli@waveaccounting.com>
2016-11-11 22:26:52 -0500

    wrong return code

--- read_fixed_len_page.cc
+++ read_fixed_len_page.cc
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
     page_file.open(argv[1]);
     if (!page_file) {
         std::cout << "Error, could not find file " << argv[1] << "\n";
-        return 0;
+        return 1;
     }
 
     std::ofstream null_out("/dev/null");
